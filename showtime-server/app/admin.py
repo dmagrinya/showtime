3149aed2583083fd42d8be9c996e12cd73f3c740
Didac <didac.magrina@gmail.com>
2020-02-15 13:44:58 +0100

    Add first backend DB models: Venue, Show and Artist
    Add to backend the DB models Venue, Show, and Artist.
    This first configuration includes:
     - Defining preliminary fields
     - Setting up a web admin to manage them
    
    Solves: hello-backend https://trello.com/c/MPGTjli3

--- showtime-server/app/admin.py
+++ showtime-server/app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Venue, Artist, Show
 
-# Register your models here.
+admin.site.register(Artist)
+
+class ShowInstanceInline(admin.TabularInline):
+    model = Show
+
+
+
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    inlines = [ShowInstanceInline]
+
+@admin.register(Show)
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ('name', 'venue', 'kick_off')
+    list_filter = ('venue', 'kick_off')
+
--- showtime-server/app/models.py
+++ showtime-server/app/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+class Venue(models.Model):
+    """Model representing a venue."""
+
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String for representing the Model object."""
+
+        return self.name
+
+
+
+class Artist(models.Model):
+    """Model representing an artist."""
+
+    name = models.CharField(max_length=200)
+    owner = models.ForeignKey('auth.User', related_name='artists', on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String for representing the Model object."""
+
+        return self.name
+
+
+
+class Show(models.Model):
+    """Model representing a show."""
+
+    name = models.CharField(max_length=200)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE) 
+    artists = models.ManyToManyField(Artist) 
+    kick_off = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ['kick_off']
+
+    def __str__(self):
+        """String for representing the Model object."""
+
+        return self.name
+
+
+
