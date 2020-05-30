03017994efbb7a577eb02dd2ad8cf88cbda319f7
Didac <didac.magrina@gmail.com>
2020-05-30 09:48:46 +0200

    Install django-oaut-toolkit and configure it as default authentication class

--- showtime-server/project/settings.py
+++ showtime-server/project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oauth2_provider',
     'rest_framework',
     'app',
 ]
@@ -126,6 +127,9 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    )
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
