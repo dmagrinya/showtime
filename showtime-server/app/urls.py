11767791e72c98c8f7ae3ac2fa3d6c8d0d9b8fe4
Didac <didac.magrina@gmail.com>
2020-05-30 10:28:14 +0200

    Add auth2_provider urls

--- showtime-server/app/urls.py
+++ showtime-server/app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from app import views
 
@@ -8,6 +9,6 @@ router.register(r'artists', views.ArtistViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('api-auth', include('rest_framework.urls')),
+    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('', include(router.urls)),
 ]
