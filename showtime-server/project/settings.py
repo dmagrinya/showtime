6f75ee90898bc6c0ddab1ae8c2bf63eda68b98ea
Didac <didac.magrina@gmail.com>
2020-05-30 10:36:22 +0200

    Configure scopes and default permission class

--- showtime-server/project/settings.py
+++ showtime-server/project/settings.py
@@ -126,10 +126,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+OAUTH2_PROVIDER = {
+    'SCOPES': {'read': 'Read scope', 'write': 'Write scope'}
+}
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
